e30f33752c2e101af3e43833e2049575b9077a17
BLRHackathon03 <github.blrhackathon03@indiumsoft.com>
2024-11-23 16:12:40 +0530

    Initial commit

---
+++ StockAPIGateway/src/test/java/com/indium/StockAPIGateway/StockApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indium.StockAPIGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
